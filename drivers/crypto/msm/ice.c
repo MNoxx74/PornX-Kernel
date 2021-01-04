b0626dc882248930ce77ab4c334ef8120a287624
TheMalachite <eliasgheeraert@gmail.com>
2021-01-04 14:55:01 +0100

    drivers: Import Xiaomi changes from lime-q-oss

--- drivers/crypto/msm/ice.c
+++ drivers/crypto/msm/ice.c
@@ -3,6 +3,7 @@
  * QTI Inline Crypto Engine (ICE) driver
  *
  * Copyright (c) 2014-2020, The Linux Foundation. All rights reserved.
+ * Copyright (C) 2020 XiaoMi, Inc.
  */
 
 #include <linux/module.h>
